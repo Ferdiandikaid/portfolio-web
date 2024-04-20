819a9dac0cb790b5de05e651955d3e47df7d0f6a
Andhika Ferdiansyah <mulyadipermadi21@gmail.com>
2024-04-20 23:38:08 +0800

    Test git: coba tambah paragraf

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,6 +6,7 @@
   <title>Welcome</title>
 </head>
 <body>
-  <h1>Hello Worlds</h1>
+  <h1>Hello World</h1>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, laborum omnis? Ad optio similique natus recusandae doloribus accusantium magnam sunt perferendis, a ducimus distinctio incidunt quae ullam, mollitia odio quibusdam!</p>
 </body>
 </html>
